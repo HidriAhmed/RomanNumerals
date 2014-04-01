c37295813be7bd635d6f6e3d342af41f7eb92e4a
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-04-01 14:39:27 +0200

    add method to convert from Roman Numerals to normal digits

--- src/main/java/com/olbati/romanumerals/RomanNumeralsConverter.java
+++ src/main/java/com/olbati/romanumerals/RomanNumeralsConverter.java
@@ -89,4 +89,79 @@ public class RomanNumeralsConverter {
 
 		return result.toString();
 	}
+
+	public int fromRomanNumeralsToNormalDigit(String romanNumerals) {
+		int result = 0;
+
+		// Converting numbers between 1 qnd 10
+		if (romanNumerals.contains("IX")) {
+			result += 9;
+		} else if (romanNumerals.contains("VIII")) {
+			result += 8;
+		} else if (romanNumerals.contains("VII")) {
+			result += 7;
+		} else if (romanNumerals.contains("VI")) {
+			result += 6;
+		} else if (romanNumerals.contains("IV")) {
+			result += 4;
+		} else if (romanNumerals.contains("V")) {
+			result += 5;
+		} else if (romanNumerals.contains("III")) {
+			result += 3;
+		} else if (romanNumerals.contains("II")) {
+			result += 2;
+		} else if (romanNumerals.contains("I")) {
+			result += 1;
+		}
+
+		// Converting numbers between 10 qnd 100
+		if (romanNumerals.contains("XC")) {
+			result += 90;
+		} else if (romanNumerals.contains("LXXX")) {
+			result += 80;
+		} else if (romanNumerals.contains("LXX")) {
+			result += 70;
+		} else if (romanNumerals.contains("LX")) {
+			result += 60;
+		} else if (romanNumerals.contains("XL")) {
+			result += 40;
+		} else if (romanNumerals.contains("L")) {
+			result += 50;
+		} else if (romanNumerals.contains("XXX")) {
+			result += 30;
+		} else if (romanNumerals.contains("XX")) {
+			result += 20;
+		} else if (romanNumerals.contains("X")) {
+			result += 10;
+		}
+		// Converting numbers between 100 qnd 1000
+		if (romanNumerals.contains("CM")) {
+			result += 900;
+		} else if (romanNumerals.contains("DCCC")) {
+			result += 800;
+		} else if (romanNumerals.contains("DCC")) {
+			result += 700;
+		} else if (romanNumerals.contains("DC")) {
+			result += 600;
+		} else if (romanNumerals.contains("CD")) {
+			result += 400;
+		} else if (romanNumerals.contains("D")) {
+			result += 500;
+		} else if (romanNumerals.contains("CCC")) {
+			result += 300;
+		} else if (romanNumerals.contains("CC")) {
+			result += 200;
+		} else if (romanNumerals.contains("C")) {
+			result += 100;
+		}
+
+		// Converting thousands
+		int nbThousands = 0;
+		if (romanNumerals.contains("CM")) {
+			nbThousands = romanNumerals.indexOf("CM");
+		} else
+			nbThousands = romanNumerals.lastIndexOf("M");
+		result += nbThousands * 1000;
+		return result;
+	}
 }
