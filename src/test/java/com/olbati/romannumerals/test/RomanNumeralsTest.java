db7fcc30565fb8b0f87d4a0f2fba232487b06384
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-04-01 13:55:41 +0200

    RomanNumeralsTest : Change method Name of the converter and add comments

--- src/test/java/com/olbati/romannumerals/test/RomanNumeralsTest.java
+++ src/test/java/com/olbati/romannumerals/test/RomanNumeralsTest.java
@@ -2,22 +2,34 @@ package com.olbati.romannumerals.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.olbati.romanumerals.RomanNumeralsConverter;
 
 public class RomanNumeralsTest {
 
+	RomanNumeralsConverter converter;
+
+	@Before
+	public void init() {
+		converter = new RomanNumeralsConverter();
+	}
+
+	/**
+	 * tests the conversion from normal digits to Roman Numerals
+	 */
 	@Test
 	public void testConvertsFromNormalDigitToRomanNumerals() {
-		RomanNumeralsConverter converter = new RomanNumeralsConverter();
+
 		int intValue = 2751;
 		String expectedRomanValue = "MMDCCLI";
 		String resultRomanValue = converter
-				.FromNormalDigitToRomanNumerals(intValue);
+				.fromNormalDigitToRomanNumerals(intValue);
 		System.out.println(intValue + " = " + resultRomanValue);
 
 		assertThat(expectedRomanValue).isEqualTo(resultRomanValue);
 
 	}
+
 }
